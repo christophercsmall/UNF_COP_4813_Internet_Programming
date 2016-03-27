e23d79148d90b46511c9429c9551bbe4e44b0257
Christopher Small <christopher.c.small@gmail.com>
2016-03-27 04:05:53 -0400

    started modify stock

--- public_html/cop4813/assign5/manager.php
+++ public_html/cop4813/assign5/manager.php
@@ -75,9 +75,95 @@ session_start();
 	</div>
 	
 	<div  id='modifyStock' style='display:none'>
-	
-	
-	
+		<form action='modifyStock.php' method='post' id='form'>		
+			<table style='text-align:center;'>
+				<tr style='text-decoration: underline; font-family: sans-serif;'>					
+					<th>MODIFY</th>
+					<th>Ticker</th>
+					<th>Value</th>
+					<th>Trade Date</th>
+					<th>Trade Time</th>
+					<th>Change</th>
+					<th>Open Value</th>
+					<th>Day's High</th>
+					<th>Days's Low</th>
+					<th>Volume</th>
+					<th>Account</th>
+				</tr>
+				
+				<?php			
+				$portf_fp = fopen("stocks.txt", 'r');
+				$acct_fp = fopen("account.txt", 'r');
+				
+				$portf = array();	
+				$acct = array();
+				
+				while($portf_line = fgets($portf_fp))
+				{
+					$stock = array();
+					
+					$tok_tick = str_replace('"', '', strtok($portf_line, ","));
+					$tok_l1 = strtok(",");
+					$tok_d1 = strtok(",");
+					$tok_t1 = strtok(",");
+					$tok_c1 = strtok(",");
+					$tok_o = strtok(",");
+					$tok_h = strtok(",");
+					$tok_g = strtok(",");
+					$tok_v = strtok(",");
+					
+					array_push($stock, $tok_tick);
+					array_push($stock, $tok_l1);
+					array_push($stock, $tok_d1);
+					array_push($stock, $tok_t1);
+					array_push($stock, $tok_c1);
+					array_push($stock, $tok_o);
+					array_push($stock, $tok_h);
+					array_push($stock, $tok_g);
+					array_push($stock, $tok_v);
+
+					array_push($portf, $stock);
+				}
+				while($acct_line = fgets($acct_fp))
+				{
+					array_push($acct, $acct_line);
+				}
+				fclose($portf_fp);
+				fclose($acct_fp);
+				sort($portf);
+				sort($acct);
+				
+				foreach($portf as $item)
+				{
+					echo "<tr>
+							<td>
+								<input type='submit' class='textContent textInput' style='margin:0; font-weight:bolder; font-size:large; width:200px;' name='modify' value='MODIFY | $item[0]' />	
+							</td>";
+							
+						foreach($item as $key => $value)
+						{
+							echo "<td>
+									$value
+								</td>";
+						}
+						
+						foreach($acct as $acct_line)
+						{
+							$acct_tick = strtok($acct_line, ",");
+							$acct_amt = strtok(",");
+							
+							if('"'.$item[0].'"' == $acct_tick)
+							{
+								echo "<td>
+										$acct_amt
+									</td>";
+							}
+						}												
+					echo "</tr>";
+				}
+				?>
+			</table>			
+		</form>
 	</div>
 	
 	<div  id='deleteStock' style='display:none'>
